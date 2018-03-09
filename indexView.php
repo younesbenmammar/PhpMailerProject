a3e5ec17b49bb197cbd745ebce9d473dcc117ebb
antoine <bergamoantoine@gmail.com>
2018-03-09 15:56:57 +0100

    commitfront

--- indexView.php
+++ indexView.php
@@ -8,7 +8,7 @@ ob_start();
 ?>
             <div align="center">
             
-            <h1>Envoyer un mail </h1><br><br>
+            <h1>Envoyer un mail </h1><br><br><br><br>
             
             
             
@@ -19,32 +19,42 @@ ob_start();
             
             <form class="" method="post" action="indexController.php" enctype="multipart/form-data">
             
+                
+            <p><strong>Votre Adresse mail</strong></p>    
             <div class="form-group">
-            <label for="receveur" class="cols-sm-2 control-label">Son Adresse mail</label>
-            <div class="cols-sm-10">
+            <label for="receveur" class="cols-sm-2 control-label"></label>
+            <div class="cols-md-offset-1 col-md-3">
             <div class="input-group">
             <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
             <input type="text" class="form-control" name="receveur" id="receveur"  placeholder="Entrez son adresse mail"/>
-            </div>
-            </div>
-            </div>
-            
-            <div class="form-group">
-            <label for="message" class="cols-sm-2 control-label">Votre message</label>
-            <div class="cols-sm-10">
-            <div class="input-group">
-            <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-            <input type="text" class="form-control" name="message" id="message"  placeholder="Entrer votre Message"/>
 </div>
 </div>
 </div>
-
-
-
-
-						<div class="form-group ">
+            <br><br>
+               
+                
+                <div class="form-group ">
                             <button id="button" class="btn btn-primary btn-lg btn-block login-button">Envoyer</button>
 						</div>
+           
+                
+                
+               <p><strong>Votre message </strong></p>
+            <div class="form-group">
+            <label for="message" class=" .col-xs-6 .col-sm-3 control-label"></label>
+            <div class="col-xs-6 .col-sm-10">
+            <div class="textarea-group">
+            <span class="textarea-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+            <textarea type="text" class="form-control" name="message" id="message"  placeholder="Entrer votre Message"/>
+                
+                
+                
+</div>
+</div>
+</div>
+
+
+						
 
 					</form>
 				</div>
--- template.php
+++ template.php
@@ -8,6 +8,7 @@
     href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
     integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
     crossorigin="anonymous">
+<link href="style/template.css" media="all" rel="stylesheet" type="text/css" />        
 </head>
 <body>
 <?= $content ?>
