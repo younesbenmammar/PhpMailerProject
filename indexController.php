514448b72fceedf6bee1c2408a9e8e7527a5fe77
younesbenmammar <younescomptepro@gmail.com>
2018-03-09 11:31:53 +0100

    first commit

---
+++ indexController.php
@@ -0,0 +1,47 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception ;
+use PHPMailer\PHPMailer\SMTP;
+include 'vendor/autoload.php';
+
+
+
+
+$receveur = $_POST["receveur"];
+$message = $_POST["message"];
+$file = $_POST["file"];
+
+$mail = new PHPMailer(true); // instancie un new objet phpmailer
+
+try {
+    
+    //$mail->SMTPDebug = 2 ;
+    $mail->isSMTP();
+    
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth  =  true ;
+    $mail->Username = 'younescomptepro@gmail.com';
+    $mail->Password = 'algeriendu34';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+    $mail->setFrom($receveur);
+    $mail->addAddress($receveur);     // Add a recipient
+    
+    $mail->addAttachment($file);         // Add attachments
+    
+    
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Planning de la Semaine';
+    $mail->Body    = $message;
+    $mail->AltBody = 'Bienvenue a tous';
+    $mail->send();
+    echo 'Message Envoyé';
+    
+}
+catch (Exception $e) {
+    echo 'Message non envoyé ' , $mail->ErrorInfo;
+}
+require 'indexView.php';
+
+?>
---
+++ indexView.php
@@ -0,0 +1,69 @@
+<?php 
+$title = 'Ymail';
+
+?>
+
+<?php 
+ob_start();
+?>
+            <div align="center">
+            
+            <h1>Envoyer un mail </h1><br><br>
+            
+            
+            
+            <div class="container">
+            <div class="row main">
+            <div class="main-login main-center">
+            
+            
+            <form class="" method="post" action="indexController.php" enctype="multipart/form-data">
+            
+            <div class="form-group">
+            <label for="receveur" class="cols-sm-2 control-label">Son Adresse mail</label>
+            <div class="cols-sm-10">
+            <div class="input-group">
+            <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+            <input type="text" class="form-control" name="receveur" id="receveur"  placeholder="Entrez son adresse mail"/>
+            </div>
+            </div>
+            </div>
+            
+            <div class="form-group">
+            <label for="message" class="cols-sm-2 control-label">Votre message</label>
+            <div class="cols-sm-10">
+            <div class="input-group">
+            <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+            <input type="text" class="form-control" name="message" id="message"  placeholder="Entrer votre Message"/>
+</div>
+</div>
+</div>
+
+            <div class="form-group">
+            <label for="file" class="cols-sm-2 control-label">Votre Fichier</label>
+            <div class="cols-sm-10">
+            <div class="input-group">
+            <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+            <input type="file" class="form-control" name="file" id="file"/>
+</div>
+</div>
+</div>
+
+
+						<div class="form-group ">
+                            <button id="button" class="btn btn-primary btn-lg btn-block login-button">Envoyer</button>
+						</div>
+
+					</form>
+				</div>
+			</div>
+		</div>
+
+		</div>
+		
+<?php 
+$content = ob_get_clean()
+?>
+<?php 
+require 'template.php';
+?>
